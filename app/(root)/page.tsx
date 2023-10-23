ccb35124d3d569bcfc185f8ad71266dc207724c9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-23 03:18:14 +0300

    Rendered the modal and added some code to take care of the hydration issues

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
+'use client'
+import Modal from '@/components/Modal/Modal'
 import { UserButton } from '@clerk/nextjs'
+import { useEffect, useState } from 'react'
 
 const SetUpPage = () => {
+	const [isRendered, setIsRendered] = useState(false)
+
+	useEffect(() => {
+		setIsRendered(true)
+	}, [])
+
+	if (!isRendered) return null
+
 	return (
 		<main>
 			<UserButton afterSignOutUrl='/' />
+			<Modal
+				isOpen
+				onClose={() => {}}
+				title='Test'
+				description='Test Description'
+			>
+				Children
+			</Modal>
 		</main>
 	)
 }
