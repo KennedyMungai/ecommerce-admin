0edb48b94aa3e8ae5f9bd6b570778174c5b1c3a4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-23 04:31:50 +0300

    Added the imports for some of the redux hooks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
 'use client'
+import { useAppDispatch, useAppSelector } from '@/Redux/hooks'
 import { UserButton } from '@clerk/nextjs'
 
 const SetUpPage = () => {
+	const isModalOpen = useAppSelector((state) => state.modalToggle.isOpen)
+	const dispatch = useAppDispatch()
+
 	return (
 		<main>
 			<UserButton afterSignOutUrl='/' />
