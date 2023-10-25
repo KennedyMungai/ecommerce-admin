8111b9e0adf387ea0039e0b0f843f0b17c0c21e3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-25 03:12:59 +0300

    Added the FormMessage component

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -8,7 +8,8 @@ import {
 	FormControl,
 	FormField,
 	FormItem,
-	FormLabel
+	FormLabel,
+	FormMessage
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -67,6 +68,7 @@ const ModalProvider = (props: Props) => {
 												{...field}
 											/>
 										</FormControl>
+										<FormMessage />
 									</FormItem>
 								)}
 							/>
